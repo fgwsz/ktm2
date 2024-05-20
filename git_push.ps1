712ca83482e19717e6fd6298cfe7f5d78a50171b
fgwsz <3191000458@qq.com>
2024-05-20 09:38:03 +0800

    change config.json

--- git_push.ps1
+++ git_push.ps1
@@ -2,6 +2,6 @@ git add ./ktm/*.py
 git add ./ktm/*.json
 git add ./git_push.sh
 git add ./git_push.ps1
-read -p "Input Git Commit Info: " commit_info
+$commit_info=Read-Host -prompt "Input Git Commit Info"
 git commit -m $commit_info
 git push
